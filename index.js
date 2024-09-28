9b11ceb9991b05bd0603998c9b726ef77f20b384
bymudboy <luis.high@hotmail.com>
2024-09-28 14:47:05 -0400

    commite projeto xpHeroi

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello");
+let nome = "Felipão"
+let xpHeroi = 10000
+
+if (xpHeroi <= 1000) {
+    console.log(nome + " de acordo com seu xp de Herói, seu elo é FERRO")
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000){
+    console.log(nome + " de acordo com seu xp de Herói, seu elo é BRONZE")
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000){
+    console.log(nome + " de acordo com seu xp de Herói, seu elo é PRATA")
+} else if (xpHeroi >= 6001 && xpHeroi <= 7000){
+    console.log(nome + " de acordo com seu xp de Herói, seu elo é OURO")
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000){
+    console.log(nome + " de acordo com seu xp de Herói, seu elo é PLATINA")
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000){
+    console.log(nome + " de acordo com seu xp de Herói, seu elo é ASCENDENTE")
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000){
+    console.log(nome + " de acordo com seu xp de Herói, seu elo é IMORTAL")
+} else if (xpHeroi >= 10001){
+    console.log(nome + " de acordo com seu xp de Herói, seu elo é RADIANTE")
+}
+
+console.log ("PARABÉNS PELO ELO ATINGIDO " + nome + " !!!")
+
+
+
+
+
+
+
+
